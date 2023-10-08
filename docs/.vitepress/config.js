7b535080e1404eabe10641c1496342d8fb695087
Jack Verstappen <jack.verstappen@hotmail.com>
2023-10-08 17:47:47 +0200

    initial vitepress site

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,49 @@
+// .vitepress/config.js
+export default {
+    title: 'upfx.app',
+    head: [['link', { rel: 'icon', href: '/favicon.ico' }]],
+    themeConfig: {
+    logo: "/logo.png",
+    siteTitle: "upfx.app",
+    nav: [
+        { text: "SUPPORT", link: "https://upfxapp.atlassian.net/servicedesk/customer/portal/1" },
+    ],
+    sidebar: [
+     {
+      text: "Section A",
+      collapsed: true,
+      collapsible: true,
+      items: [
+        { text: "Introduction", link: "/introduction" },
+        { text: "Getting Started", link: "/getting-started" },
+      ],
+    },
+    {
+      text: "Section B",
+      collapsed: true,
+      items: [
+        { text: "Introduction", link: "/introduction" },
+        { text: "Getting Started", link: "/getting-started" },
+      ],
+    },
+    {
+      text: "Section C",
+      items: [
+        { text: "Introduction", link: "/introduction" },
+        { text: "Getting Started", link: "/getting-started" },
+      ],
+    },
+    
+    ],
+    socialLinks: [
+        { icon: "youtube", link: "https://youtube.com/@upfxapp" },
+        { icon: "twitter", link: "https://twitter.com/company/upfxapp" },
+    ],
+    footer: {
+        message: "Developed by (} digitizedbeing",
+        copyright: "Copyright © 2022-present upfx.app",
+    },
+    description: 'Apps to get more out of Jira',
+  },
+ 
+}
---
+++ docs/index.md
@@ -0,0 +1,12 @@
+---
+layout: home
+
+hero:
+  name: upfx
+  text: org boost for Jira
+  image:
+    src: /logo.png
+    alt: upfx.app logo
+  tagline: allow your organisation to get out of Jira it needs and deserves... 
+
+---
